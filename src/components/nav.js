88cb97caf452895312dd54ca488075fda1869b69
Daniel Goebel <dgoebel@uchicago.edu>
2024-08-20 13:28:35 -0500

    changes to the nav to make it mobile friendly

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,61 +1,113 @@
-import * as React from "react";
-import { useStaticQuery, graphql, Link } from "gatsby";
-import { Container, Row, Col } from "react-bootstrap";
-import Header from "./header";
-import "./layout.css";
+import React, { useState, useEffect } from "react";
+import { Link } from "gatsby";
+import { Container, Row, Button } from "react-bootstrap";
 import Logo from "../images/klab-logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark} from "@fortawesome/free-solid-svg-icons";
+import "./layout.css";
 import "../components/gridder.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Nav = () => {
+  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  // Function to toggle the mobile menu visibility
+  const toggleMobileMenu = () => {
+    setIsMobileMenuVisible(!isMobileMenuVisible);
+  };
+
+  // Function to check if the viewport is below 576px
+  const handleResize = () => {
+    setIsMobileView(window.innerWidth < 767);
+  };
+
+  useEffect(() => {
+    handleResize(); // Check the viewport size on component mount
+    window.addEventListener("resize", handleResize); // Add resize event listener
+    return () => {
+      window.removeEventListener("resize", handleResize); // Cleanup event listener on component unmount
+    };
+  }, []);
+
+  const mobileButtonStyle = {
+    width: "50px",
+    height: "50px",
+    alignSelf: "flex-start",
+    marginTop: "15px",
+    backgroundColor: "var(--color-secondary)",
+    outline: "none",
+    border: "none"
+  }
   const navStyles = {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   };
+  const navLogoStyleMobile = {
+    width: "50%",
+    paddingTop: "1rem",
+    paddingBottom: "1rem",
+    flexGrow: "1",
+    marginRight: "25cqw",
+  };
   const navLogoStyle = {
+    display: isMobileView  ? "none" : "flex",
     width: "23%",
     paddingTop: "1rem",
-    paddingBottom: "1rem"
+    paddingBottom: "1rem",
   };
   const navListStyles = {
-    display: "flex",
+    display: isMobileView ? (isMobileMenuVisible ? "block" : "none") : "flex",
     listStyle: "none",
     flexDirection: "row",
     justifyContent: "flex-end",
     flex: "1 0 auto",
     padding: "0",
     margin: "0",
+    
   };
   const navItemStyles = {
     margin: "0",
     padding: "0",
   };
   const navLinkStyles = {
-    padding: "clamp(10px, 1cqw, 16px)",
+    display: isMobileView ? (isMobileMenuVisible ? "block" : "none") : "flex",
+    padding: isMobileView && isMobileMenuVisible ? "clamp(9px, .5cqw, 16px)" : "clamp(10px, 1cqw, 16px)",
     textDecoration: "none",
     color: "var(--color-text)",
     textTransform: "uppercase",
-    fontSize: "clamp(9px, 1cqw, 14px)",
+    fontSize: isMobileView && isMobileMenuVisible ? "clamp(16px, 1.5cqw, 24px)" : "clamp(9px, 1cqw, 14px)",
+    // borderBottom: isMobileView && isMobileMenuVisible ? "1px dotted #333" : "none",
     margin: "0",
   };
   const containerStyle = {
-    background: "linear-gradient(0deg, rgb(235, 288, 243), rgb(198, 188, 188) 51%,transparent 0,transparent)",
+    background:
+      "linear-gradient(0deg, rgb(235, 288, 243), rgb(198, 188, 188) 51%,transparent 0,transparent)",
     width: "100vw",
     height: "1px",
     backgroundSize: "8px 1px",
-  }
+  };
 
   return (
     <>
       <Container fluid>
-        
         <Container>
-          <Row>
+          <Row style={{display: `flex`, justifyContent: `space-between`}}>
+            {isMobileView && (
+              <>
+              <Link to={`/`} style={navLogoStyleMobile}>
+              <img src={Logo} alt="Logo" />
+            </Link>
+            <Button onClick={toggleMobileMenu} style={mobileButtonStyle}>
+                <FontAwesomeIcon icon={isMobileMenuVisible ? faXmark : faBars} /> {/* Conditionally render the icon */}
+              </Button>
+              </>
+            )}
             <nav style={navStyles}>
               <Link to={`/`} style={navLogoStyle}>
-                <img src={Logo} />
+                <img src={Logo} alt="Logo" />
               </Link>
               <ul style={navListStyles}>
                 <li style={navItemStyles}>
