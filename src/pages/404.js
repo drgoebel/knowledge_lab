4ad4509f339becdf797d3a4777551fc7b51d3cae
Daniel Goebel <dgoebel@uchicago.edu>
2024-09-22 13:49:12 -0500

    more updates to styles and removing the old pages and stashing in an archive

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,61 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import Nav from "../components/nav";
+import { Container } from "react-bootstrap";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
 
-const NotFoundPage = () => (
+
+const NotFoundPage = () => {
+
+  useEffect(() => {
+    const script1 = document.createElement("script");
+
+    script1.src = "https://lab.hakim.se/blob/03/js/blob.js";
+    // "../components/blob.js";
+
+    document.body.appendChild(script1);
+  }, []);
+  return (
   <Layout>
+    <Nav />
+    <Container fluid><Container>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p><a href="/">Return home</a></p>
+
+    <div style={{ width: `100%`, height: `100%`, position: `relative` }}>
+        <p>
+          Double click to split.{" "}
+          <a id="keyboardUp" href="#">
+            Increase
+          </a>{" "}
+          /{" "}
+          <a id="keyboardDown" href="#">
+            decrease
+          </a>{" "}
+          size or{" "}
+          <a id="keyboardLeft" href="#">
+            Previous
+          </a>{" "}
+          /{" "}
+          <a id="keyboardRight" href="#">
+            Next
+          </a>{" "}
+          skin.
+        </p>
+        <canvas id="world" style={{ width: `100%`, height: `100%` }}>
+          .....
+        </canvas>
+      </div>
+    </Container></Container>
   </Layout>
-)
+  )
+}
 
 export const Head = () => <Seo title="404: Not Found" />
 
